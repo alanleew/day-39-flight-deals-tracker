74e68b0dd63e9d74579abd754b54d4092718c186
alanleew <alanleew412@gmail.com>
2025-02-03 13:30:47 -0800

    Upload

---
+++ data_manager.py
@@ -0,0 +1,13 @@
+import requests
+
+SHEETY_GET_URL = "https://api.sheety.co/789e9c01b29d4ce7279876f9f22b5a3e/flightDeals/prices"
+SHEETY_POST_URL = "https://api.sheety.co/789e9c01b29d4ce7279876f9f22b5a3e/flightDeals/prices"
+SHEETY_PUT_URL = "https://api.sheety.co/789e9c01b29d4ce7279876f9f22b5a3e/flightDeals/prices/[Object ID]"
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    sheet_request = requests.get(url=SHEETY_GET_URL)
+    json_data = sheet_request.json()
+
+    def edit_row(self):
+
